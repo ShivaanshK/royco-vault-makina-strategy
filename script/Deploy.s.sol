125cd172c8e6907b1f85f8211631e4f469682a7b
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2026-03-08 15:32:57 -0700

    deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.28;
 
 import {Script} from "lib/forge-std/src/Script.sol";
+import {DeploymentConfig} from "./config/DeploymentConfig.sol";
 import {console2} from "lib/forge-std/src/console2.sol";
+import {RoycoVaultMakinaStrategy} from "../src/RoycoVaultMakinaStrategy.sol";
+import {Create2DeployUtils} from "./utils/Create2DeployUtils.sol";
 
-contract DeployScript is Script {
+contract DeployScript is Script, Create2DeployUtils, DeploymentConfig {
+    bytes32 internal constant STRATEGY_DEPLOYMENT_SALT = keccak256(abi.encode("ROYCO_VAULT_MAKINA_STRATEGY"));
+
     function run() external virtual {
         // Read deployer private key
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
@@ -12,16 +17,36 @@ contract DeployScript is Script {
         // Read market name from config
         string memory strategyName = vm.envString("STRATEGY_NAME");
 
-        console2.log("Deploying strategy from config:", strategyName);
+        console2.log("Deploying Makina strategy from config:", strategyName);
         deployFromConfig(strategyName, deployerPrivateKey);
     }
 
     /// @notice Deploy a strategy using Solidity configuration
-    /// @param strategyName The name of the strategy to deploy (must match a config in DeploymentConfig)
-    /// @param deployerPrivateKey The private key of the deployer
+    /// @param _strategyName The name of the strategy to deploy (must match a config in DeploymentConfig)
+    /// @param _deployerPrivateKey The private key of the deployer
     /// @return strategy The deployed strategy
-    function deployFromConfig(string memory strategyName, uint256 deployerPrivateKey)
+    function deployFromConfig(string memory _strategyName, uint256 _deployerPrivateKey)
         public
         returns (address strategy)
-    {}
+    {
+        StrategyDeploymentConfig memory config = _strategyConfigs[_strategyName];
+
+        bytes memory strategyCreationCode = abi.encodePacked(
+            type(RoycoVaultMakinaStrategy).creationCode,
+            abi.encode(config.roycoFactory, config.roycoVault, config.makinaMachine, config.strategyType)
+        );
+
+        vm.startBroadcast(_deployerPrivateKey);
+
+        bool alreadyDeployed;
+        (strategy, alreadyDeployed) = _deployWithSanityChecks(STRATEGY_DEPLOYMENT_SALT, strategyCreationCode, false);
+
+        vm.stopBroadcast();
+
+        if (alreadyDeployed) {
+            console2.log(_strategyName, " Makina Strategy already deployed at:", strategy);
+        } else {
+            console2.log(_strategyName, " Makina Strategy deployed at:", strategy);
+        }
+    }
 }
--- script/config/DeploymentConfig.sol
+++ script/config/DeploymentConfig.sol
@@ -10,8 +10,17 @@ import {StrategyType} from "../../src/RoycoVaultMakinaStrategy.sol";
 abstract contract DeploymentConfig {
     uint256 internal constant MAINNET = 1;
 
-    // TODO: Update with new factory
+    string internal constant ROYCO_DAWN_SENIOR_VAULT = "DSV";
+
+    // TODO: Update with new factory once deployed
+    // Deployed using CREATE2, so the address is the same on all chain
     address internal constant ROYCO_FACTORY_ADDRESS = 0xD567cCbb336Eb71eC2537057E2bCF6DB840bB71d;
+
+    /// @dev Address of the Royco Dawn Senior Vault
+    address internal constant DSV = 0xcD9f5907F92818bC06c9Ad70217f089E190d2a32;
+
+    /// @dev Address of the DUSD Makina Machine
+    address internal constant DUSD_MAKINA_MACHINE = 0x6b006870C83b1Cd49E766Ac9209f8d68763Df721;
 
     struct StrategyDeploymentConfig {
         address roycoFactory;
@@ -26,5 +35,12 @@ abstract contract DeploymentConfig {
         _initializeStrategyConfigs();
     }
 
-    function _initializeStrategyConfigs() internal {}
+    function _initializeStrategyConfigs() internal {
+        _strategyConfigs[ROYCO_DAWN_SENIOR_VAULT] = StrategyDeploymentConfig({
+            roycoFactory: ROYCO_FACTORY_ADDRESS,
+            roycoVault: DSV,
+            makinaMachine: DUSD_MAKINA_MACHINE,
+            strategyType: StrategyType.ASYNC
+        });
+    }
 }
